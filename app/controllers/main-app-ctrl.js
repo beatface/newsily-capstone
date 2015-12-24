8a264232fb786a8a6e383f20c08b6b12068a8b99
beatface <emma.beaton@gmail.com>
2015-12-23 20:26:14 -0600

    comments are updating to firebase and content in modal

--- app/controllers/main-app-ctrl.js
+++ app/controllers/main-app-ctrl.js
@@ -108,7 +108,37 @@ app.controller("mainAppCtrl", ["$scope", "$state", "$firebaseArray", "$http", "g
 	$scope.newComment = "";
 	// ----- ADDING COMMENT TO POST'S MODAL ----- //
 	$scope.addComment = function(currentpost, newComment) {
-		console.log("adding a new comment", currentpost, newComment);
+		console.log("adding a new comment", currentpost.$id, newComment);
+
+		var now = new Date();
+		// Create an array with the current month, day and time
+		var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+		// Create an array with the current hour, minute and second
+		var time = [ now.getHours(), now.getMinutes() ];
+		// Determine AM or PM suffix based on the hour
+		var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+		// Convert hour from military time
+		time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+		// If hour is 0, set it to 12
+		time[0] = time[0] || 12;
+		// If seconds and minutes are less than 10, add a zero
+		for ( var i = 1; i < 3; i++ ) {
+		    if ( time[i] < 10 ) {
+		        time[i] = "0" + time[i];
+		    }
+		}
+		// Return the formatted string
+		var formattedDate = date.join("/") + " " + time.join(":") + " " + suffix;
+
+		var commentObj = {
+			content: newComment,
+			postedBy: userProfileData.username,
+			postDate: formattedDate
+		}
+
+		var ref = new Firebase('https://newsily.firebaseio.com/posts/' + currentpost.$id + "/comments");
+		refArray = $firebaseArray(ref);
+		refArray.$add(commentObj);
 	};
 
 
