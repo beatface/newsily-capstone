2dd5c5c96d9d9401b643aa4f4b32f81f0612a357
beatface <emma.beaton@gmail.com>
2016-01-04 14:08:37 -0600

    started refactor from angularfire to regular firebase methods. got posts sorting and populating by timestamp with newest post showing first

--- app/controllers/main-app-ctrl.js
+++ app/controllers/main-app-ctrl.js
@@ -6,11 +6,21 @@ app.controller("mainAppCtrl", ["$scope", "$state", "$firebaseArray", "$http", "g
 	// --- ALL POSTS FROM FIREBASE
 	var allPostsRef = new Firebase("https://newsily.firebaseio.com/posts");
 	// setting all posts variable on the scope for loading into partial
-	var postsRef = $firebaseArray(allPostsRef);
-	$scope.posts = allPostsRef;
+	allPostsRef.on('value', function(snapshot) {
+		console.log("all posts snapshot", snapshot.val());
+		$scope.postsObj = snapshot.val();
+
+		$scope.postsArr = [];
+
+		for (var item in $scope.postsObj){
+		    $scope.postsArr.push($scope.postsObj[item]);
+		}
+	});
+
 
 	// --- ALL FAVOURITES FROM FIREBASE
 	var allFavsRef = new Firebase("https://newsily.firebaseio.com/favourites");
+
 
 	// --- CURRENT USER DATA
 	var currentUser = currentUserData.getUserData();
@@ -21,22 +31,20 @@ app.controller("mainAppCtrl", ["$scope", "$state", "$firebaseArray", "$http", "g
 
 	//for accessing user's profile data
 	var userProfileData = new Firebase("https://newsily.firebaseio.com/users/" + currentUser.auth.uid);
-	userProfileData = $firebaseObject(userProfileData);
-	$scope.currentUserProfileData = userProfileData;
-	console.log("userProfileData", userProfileData);
+	userProfileData.on('value', function(snapshot) {
+		$scope.currentUserProfileData = snapshot.val();
+	});
 
 	// for loading group names into sidebar menu (uses both user's groups and all groups -below- to filter)
 	var userGroupsRef = new Firebase("https://newsily.firebaseio.com/users/" + currentUser.auth.uid + "/joined_groups");
 	$scope.usersGroups = $firebaseArray(userGroupsRef);
-	// console.log("user's groups", $scope.usersGroups);
 
 	// for loading group names into sidebar menu
 	var allGroupsRef = new Firebase("https://newsily.firebaseio.com/groups");
 	$scope.allGroups = $firebaseArray(allGroupsRef);
-	// console.log("all groups", $scope.allGroups);
 
 
-    // getting user's joined groups and setting group factory with first group in the array
+    // getting user's joined groups and setting group factory with first group in the array for inital page view on login
     userGroupsRef.on("value", function(snapshot) {
         var groups = snapshot.val();
         var key = _.findKey(groups);  
@@ -69,10 +77,9 @@ app.controller("mainAppCtrl", ["$scope", "$state", "$firebaseArray", "$http", "g
 		}
 		// Return the formatted string
 		var formattedDate = date.join("/") + " " + time.join(":") + " " + suffix;
-		
+
 		$http.get("http://api.embed.ly/1/extract?key=514b5e76363e48c7892110e2bd33a491&url=" + $scope.url + "&maxwidth=500")
 		.then(function(data) {
-			console.log("data", data);
 			// create object for upload with relevant info
 			var dataForFirebase = {
 				description: data.data.description,
@@ -80,19 +87,16 @@ app.controller("mainAppCtrl", ["$scope", "$state", "$firebaseArray", "$http", "g
 				url: data.data.original_url,
 				title: data.data.title,
 				group: $scope.currentGroupView,
+				postTimeStamp: new Date().getTime(),
 				postDate: formattedDate,
-				postedBy: userProfileData.username,
+				postedBy: $scope.currentUserProfileData.username,
 				postedByUid: currentUser.auth.uid
 			};
 			// push object to posts in firebase
-			postsRef.$add(dataForFirebase)
-			.then(function(refinfo) {
-				console.log("refinfo", refinfo);
-			});
+			allPostsRef.push(dataForFirebase);
+			$scope.url = ""; // resets input field to blank
 		});
 	};
-
-
 
 
 	// ----- CLICK FUNCTIONS ON BODY FOR DYNAMICALLY LOADED MENU ITEMS AND POSTS ----- // 
@@ -142,16 +146,16 @@ app.controller("mainAppCtrl", ["$scope", "$state", "$firebaseArray", "$http", "g
 		}
 		// Return the formatted string
 		var formattedDate = date.join("/") + " " + time.join(":") + " " + suffix;
-
 		var commentObj = {
 			content: newComment,
 			postedBy: userProfileData.username,
 			postDate: formattedDate
 		};
-
 		var ref = new Firebase('https://newsily.firebaseio.com/posts/' + currentpost.$id + "/comments");
 		refArray = $firebaseArray(ref);
 		refArray.$add(commentObj);
+
+		$scope.newComment = ""; // resets input field to blank
 	};
 
 
